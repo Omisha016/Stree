f36e595a344c57cf8a03f14686dfe54b6952551e
hiya dey sarkar <hiya.deysark@gmail.com>
2021-08-15 17:15:41 +0530

    bugs fixed

--- navigation.js
+++ navigation.js
@@ -1,41 +1,40 @@
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
+const invisibleAfterLogIn = document.querySelectorAll('.logged-out-invisible');
 
-const setupUI = (user) => {
-
-    if(user){
-        //toggle UI elements
-
-        console.log('logged in');
-
-        loggedInLinks.forEach(item => item.style.display = 'block');
-        loggedOutLinks.forEach(item => item.style.display = 'block');
-    } else{
-        //toggle UI elements
-
-        console.log('logged out');
-
-        loggedInLinks.forEach(item => item.style.display = 'none');
-        loggedOutLinks.forEach(item => item.style.display = 'block');
-    }
-}
 
 firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION).then(() => {
     auth.onAuthStateChanged(function(user) {
 
-        console.log(user);
-
-        if(user)
-        {
-            console.log('logged in xx');
-            setupUI(user);
-        }
-        else
-        {
-            console.log('logged out xx');
-            setupUI(user);
+        if(user){
+            //toggle UI elements
+    
+            console.log('logged in');
+    
+            loggedInLinks.forEach(item => item.style.display = 'block');
+            loggedOutLinks.forEach(item => item.style.display = 'block');
+            invisibleAfterLogIn.forEach(item => item.style.display = 'none');
+        } else{
+            //toggle UI elements
+    
+            console.log('logged out');
+    
+            loggedInLinks.forEach(item => item.style.display = 'none');
+            loggedOutLinks.forEach(item => item.style.display = 'block');
+            invisibleAfterLogIn.forEach(item => item.style.display = 'block');
         }
     });
 });
 
-//setupUI();
+
+function buycheck () {
+    auth.onAuthStateChanged(function(user){
+        if(user){
+            window.location = 'price.html';
+        }
+        else 
+        {
+            window.alert('You need to Login first to buy');
+        }
+    })
+};
